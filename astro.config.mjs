814b4fe958ba8c333328d2d71e7d0127185e66c2
Dexter <raultasayco54321@gmail.com>
2025-09-20 17:51:45 -0500

    second

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-// @ts-check
+
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
 
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  site: 'https://github.com/F1nTrack/landingPagePrueba',
+  integrations: [tailwind(), react()],
+  site: 'https://f1ntrack.github.io/landingPagePrueba',
+  base: '/landingPagePrueba/',
 });
