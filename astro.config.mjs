7957364c6158d7300d1d978fe9fb17e37b7cc5a2
Dexter <raultasayco54321@gmail.com>
2025-09-20 17:45:35 -0500

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
+  integrations: [react(), tailwind()],
+  site: 'https://github.com/F1nTrack/landingPagePrueba',
 });
